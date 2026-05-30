5164703414ad205a18405cde21c6d9d6c36055d2
vzqykfzwbt-png <ame.redemarrer.5z@icloud.com>
2026-05-30 14:34:55 +0200

    déplacement sans animation

--- peronnage_scène.gd
+++ peronnage_scène.gd
@@ -1,22 +1,35 @@
 extends CharacterBody3D
 
+@onready var camera_mount = $camera_mount
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var sens_horizontal = 0.5
+var sens_vertical = 0.5
+const CAMERA_MIN_ANGLE = -40
+const CAMERA_MAX_ANGLE = 30
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
+		camera_mount.rotate_x(deg_to_rad(event.relative.y * sens_vertical))
+		camera_mount.rotation.x = clamp(
+			camera_mount.rotation.x,
+			deg_to_rad(CAMERA_MIN_ANGLE),
+			deg_to_rad(CAMERA_MAX_ANGLE)
+		)
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("q", "d", "s", "z")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
